76808ceaaeacb62ea067d9a4ca1f35fd05c418ce
techcristian <100042922+techcristian@users.noreply.github.com>
2025-07-18 13:26:47 -0300

    firebase mod

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,14 +1,27 @@
 import admin from 'firebase-admin';
+import dotenv from 'dotenv';
+dotenv.config();
 
-if(!admin.apps.length){
- const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON) 
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // importante
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+   universe_domain:process.env.UNIVERSE_DOMAIN
+};
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 }
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
 const db = admin.firestore();
-
 
 export { db };
